838f59473f27e26fd198eb84006412df0cfd2808
Yongseok1991 <dobest0998@gmail.com>
2021-07-16 22:19:21 +0900

    환경세팅 완료

---
+++ blog/src/main/java/com/cos/blog/controller/BlogController.java
@@ -0,0 +1,18 @@
+package com.cos.blog.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.NoArgsConstructor;
+
+@RestController
+@NoArgsConstructor
+
+public class BlogController {
+	
+	@GetMapping("/test/hello")
+	public String hello() {
+		return "<h1>hello spring boot</h1>";
+	}
+} // end class
